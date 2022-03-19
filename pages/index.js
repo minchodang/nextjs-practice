30812bcddaa10df349a5d2473a29f0c939637822
minsukang <minsu910725@gmail.com>
2022-03-19 20:43:23 +0900

    Add: 상품 상세 페이지 및 다이나믹 라우츠 연동

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
+import axios from 'axios';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css';
+import { Divider, Header } from 'semantic-ui-react';
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const API_URL =
+    'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+  function getData() {
+    axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    });
+  }
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <Head>
         <title>Home | 민수</title>
       </Head>
-      <div className={styles.container}>create-next-app으로 설치하면</div>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <ItemList list={list.slice(0, 9)} />
+      <Divider />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <ItemList list={list.slice(9)} />
     </div>
   );
 }
