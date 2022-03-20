f193ea4c77289d741e6008446a17193cf88daffc
minsukang <minsu910725@gmail.com>
2022-03-20 12:02:10 +0900

    Add: 서버사이드 랜더링 적용

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css';
-import { Divider, Header } from 'semantic-ui-react';
+import { Divider, Header, Loader } from 'semantic-ui-react';
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const API_URL =
     'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
   function getData() {
     axios.get(API_URL).then((res) => {
       console.log(res.data);
       setList(res.data);
+      setIsLoading(false);
     });
   }
   useEffect(() => {
@@ -22,16 +25,28 @@ export default function Home() {
     <div>
       <Head>
         <title>Home | 민수</title>
+        <meta name="description" content="민수 홈 입니다."></meta>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <ItemList list={list.slice(0, 9)} />
-      <Divider />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <div style={{ padding: '300px 0' }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <ItemList list={list.slice(0, 9)} />
+          <Divider />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
--- pages/view/[id].js
+++ pages/view/[id].js
@@ -2,28 +2,35 @@ import Axios from 'axios';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Item from '../../src/component/Item';
+import { ItemHeader, Loader } from 'semantic-ui-react';
+import Head from 'next/head';
 
-const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const [item, setItem] = useState({});
-
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-
-  function getData() {
-    Axios.get(API_URL).then((res) => {
-      setItem(res.data);
-    });
-  }
-
-  useEffect(() => {
-    if (id && id > 0) {
-      getData();
-    }
-  }, [id]);
-
-  return <Item item={item} />;
+const Post = ({ item }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Post;
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+  return {
+    props: {
+      item: data,
+    },
+  };
+}
